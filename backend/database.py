748b5f36fef5f33578f0a780d2d4cd252a8fa8aa
kiran krishnan <kirankrishnan910@gmail.com>
2026-03-11 13:04:54 +0530

    added docker

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,13 @@
+import os
 from motor.motor_asyncio import AsyncIOMotorClient
 
-MONGO_URL = "mongodb://localhost:27017"
+MONGO_URL = os.environ.get("MONGO_URL", "mongodb://localhost:27017")
 
 client = AsyncIOMotorClient(MONGO_URL)
 
 db = client["school_db"]
 
-users_collection = db["users"]
+users_collection    = db["users"]
 students_collection = db["students"]
-marks_collection = db["marks"]
-leave_collection = db["leave_requests"]
+marks_collection    = db["marks"]
+leave_collection    = db["leave_requests"]
